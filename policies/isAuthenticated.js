656bc77cae4bc7dd91748368d6bf17ae0859d3c9
Fredrik Söderström <tirithen@gmail.com>
2014-05-21 20:29:19 +0200

    Add 401 response if not logged in

--- policies/isAuthenticated.js
+++ policies/isAuthenticated.js
@@ -2,6 +2,9 @@ module.exports = function (req, res, next) {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.redirect('/login');
+    res.status(401).send(
+      'Unauthorized, please <a href="/login">login</a> brefore visiting this address.' +
+      '<script>window.location.href="/login"</script>'
+    );
   }
 };
