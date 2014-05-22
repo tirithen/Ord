1963a32ddb0df2f72ca70913ff88b66c39dd871c
Fredrik Söderström <tirithen@gmail.com>
2014-05-22 18:42:53 +0200

    Remove create page route, handled by frontend instead

--- controllers/AdministrationPage.js
+++ controllers/AdministrationPage.js
@@ -2,10 +2,7 @@ var services = require('../services')
   , models = require('../models');
 
 module.exports = {
-    'GET /administration/page/create': function (req, res) {
-      services.renderRes(req, res, 'page', { page: new models.Page(), rootPages: rootPages });
-    }
-  , 'GET /administration/page': function (req, res) {
+    'GET /administration/page': function (req, res) {
       services.findAllPages(function (err, pages) {
         if (err) {
           console.error(err);
