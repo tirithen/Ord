3c022dfc68fda4df1fec6e6b3067a7816e45c787
Fredrik Söderström <tirithen@gmail.com>
2014-07-11 02:08:49 +0200

    Reformat AdministrationSiteSettings controller #20

--- controllers/AdministrationSiteSettings.js
+++ controllers/AdministrationSiteSettings.js
@@ -1,30 +1,30 @@
-var fs = require('fs')
-  , path = require('path')
-  , services = require('../services')
-  , models = require('../models');
+var fs = require('fs');
+var path = require('path');
+var services = require('../services');
+var models = require('../models');
 
 module.exports = {
-    'GET /administration/siteSettings': function (req, res) {
-      var siteSettings = services.siteSettings.getAllAsObjects();
+  'GET /administration/siteSettings': function (req, res) {
+    var siteSettings = services.siteSettings.getAllAsObjects();
 
-      siteSettings.theme.options = fs.readdirSync(path.join(__dirname, '../themes'));
+    siteSettings.theme.options = fs.readdirSync(path.join(__dirname, '../themes'));
 
-      services.renderRes(
-          req
-        , res
-        , 'administration/siteSettings'
-        , {
-            page: { title: 'Site settings' }
-          , siteSettings: siteSettings
-        }
-      );
-    }
-  , 'POST /administration/siteSettings': function (req, res) {
-      /*req.body.settings.forEach(function (setting) {
-        console.log(setting);
-      });*/
-    }
-  , policies: {
-      '/^\\/administration/siteSettings/': 'isAuthenticated'
+    services.renderRes(
+      req,
+      res,
+      'administration/siteSettings',
+      {
+        page: { title: 'Site settings' },
+        siteSettings: siteSettings
+      }
+    );
+  },
+  'POST /administration/siteSettings': function (req, res) {
+    /*req.body.settings.forEach(function (setting) {
+      console.log(setting);
+    });*/
+  },
+  policies: {
+    '/^\\/administration/siteSettings/': 'isAuthenticated'
   }
-}
+};
